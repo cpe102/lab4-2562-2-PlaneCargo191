b16ad3ba1b3db78eb423f3e3f06a4619fe8eedac
PlaneCargo191 <t.james_17@hotmail.com>
2020-01-03 16:34:20 +0700

    New

--- lab4_3.cpp
+++ lab4_3.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+using namespace std;
 
+int findDivisor(int x)
+{
+    int min=2;
+    while (x%min!=0)
+    {
+        min=min+1;
+    }
+    return min;
+}
+
+int main()
+{
+    cout<<findDivisor(10)<<"\n";
+    cout<<findDivisor(97)<<"\n";
+    cout<<findDivisor(221)<<"\n";
+
+    return 0;
+}
