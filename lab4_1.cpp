7adeddfb36f7af88a6633f8c5b861a8655051cfa
PlaneCargo191 <t.james_17@hotmail.com>
2020-01-03 16:34:00 +0700

    New

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
 
+float function(float x)
+{
+    float y;
+    y=(3*pow(x, 3))+(2*2.71828)+pow(2, 2*x+1)+sqrt(pow(x, 2)+1);
+    return y;
+}
+
+int main()
+{
+    float x;
+    cout<<"Enter x =  ";
+    cin>>x;
+
+    cout<<"\nResult y =  "<<function(x);
+
+    return 0;
+}
