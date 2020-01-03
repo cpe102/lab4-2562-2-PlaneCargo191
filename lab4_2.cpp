22ec104bf5d10370e3a562f85d57e5d6a3b2a49c
PlaneCargo191 <t.james_17@hotmail.com>
2020-01-03 16:34:09 +0700

    New

--- lab4_2.cpp
+++ lab4_2.cpp
@@ -2,11 +2,18 @@
 
 using namespace std;
 
-//Write function findDistance() here
+float findDistance(float u, float a, float t)
+{
+  float s;
+  s=(u*t)+(0.5*a*t*t);
+  return s;
+}
 
-int main(){
-
-  //Calling findDistance() using test cases
+int main()
+{
+  cout<<findDistance(0, 0.5, 1)<<"\n";
+  cout<<findDistance(1.5, -1, 2)<<"\n";
+  cout<<findDistance(5, 4, 3)<<"\n";
 
   return 0;
 }
